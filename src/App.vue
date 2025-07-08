261494f119af235a28952924fbac654e2cce995c
Kewish Fagoe <fagoe.kewish@gmail.com>
2025-07-07 23:07:19 -0300

    Implemented root font-size tracking and added SetRootFontSizeModal component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,62 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue'
+
 import Heading from './components/Heading.vue';
 import Footer from './components/Footer.vue';
+import SetRootFontSizeModal from './components/SetRootFontSizeModal.vue';
 
+const rootFontSize = ref<number>(16)
+
+const loadRootFontSize = () => {
+    try {
+        const storedValue = localStorage.getItem('rootFontSize')
+        if (storedValue) {
+            rootFontSize.value = parseInt(storedValue)
+        }
+    } catch (error) {
+        console.warn('localStorage access failed:', error)
+    }
+}
+
+const updateRootFontSize = (newValue: number) => {
+    rootFontSize.value = newValue
+    try {
+        if (newValue === 16) {
+          localStorage.removeItem('rootFontSize')
+        } else {
+          localStorage.setItem('rootFontSize', newValue.toString())
+        }
+    } catch (error) {
+        console.warn('localStorage write failed: ', error)
+    }
+}
+
+
+const modalRef = ref(null)
+const showModal = () => {
+    if (modalRef.value) {
+        modalRef.value.showModal()
+    }
+}
+
+onMounted(() => {
+    loadRootFontSize()
+})
 </script>
 
 <template>
     <main class="prose">
         <Heading message="PX to REM Calculator" />
+        <p class="flex items-center">
+            The calculations are based on a root <code>font-size</code> of {{ rootFontSize }} pixel.
+            <span @click="showModal">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil ml-2 hover:cursor-pointer" viewBox="0 0 16 16">
+                    <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/>
+                </svg>
+            </span>
+        </p>
         <Footer />
+        <SetRootFontSizeModal ref="modalRef" :root-font-size="rootFontSize" @update-root-font-size="updateRootFontSize" />
     </main>
 </template>
 
---
+++ src/components/SetRootFontSizeModal.vue
@@ -0,0 +1,81 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+
+const props = defineProps<{
+    rootFontSize: number
+}>()
+
+const emits = defineEmits<{
+    updateRootFontSize: [newRootFontSize: number]
+}>()
+
+const newRootFontSize = ref<number>(props.rootFontSize)
+const showError = ref<boolean>(false)
+
+const modalRef = ref(null)
+
+function showModal() {
+    if (modalRef.value) {
+        modalRef.value.showModal()
+    }
+}
+
+function closeModal() {
+    if (modalRef.value) {
+        modalRef.value.close()
+        showError.value = false
+    }
+}
+
+function setNewRootFontSize() {
+    // check if 0, negative, or has decimals
+    if (newRootFontSize.value <= 0 || !Number.isInteger(newRootFontSize.value)) {
+        showError.value = true
+        return
+    }
+
+    closeModal()
+    emits("updateRootFontSize", newRootFontSize.value)
+}
+
+defineExpose({
+    showModal
+})
+
+const newRootFontSizeInput = ref()
+onMounted(() => {
+    // small delay to ensure the modal is fully rendered
+    setTimeout(() => {
+        newRootFontSizeInput.value!.focus()
+    }, 10)
+})
+</script>
+
+<template>
+    <dialog ref="modalRef" class="modal modal-bottom sm:modal-middle">
+        <div class="modal-box not-prose">
+            <h3 class="text-lg font-bold">Change Root Font Size</h3>
+            <label class="input w-full mt-6">
+                <input
+                    type="number"
+                    placeholder="Enter a new root font size value..."
+                    id="root-font-size-input"
+                    name="newRootFontSize"
+                    v-model="newRootFontSize"
+                    ref="newRootFontSizeInput"
+                    @keyup.enter="setNewRootFontSize"
+                />
+                <span class="label">px</span>
+            </label>
+            <div v-if="showError" class="text-red-600 text-xs mt-2">The root font size needs to be a valid whole number larger than 0.</div>
+            <div class="modal-action">
+                <button class="btn" @click="closeModal">Close</button>
+                <button class="btn btn-primary" @click="setNewRootFontSize">Save</button>
+            </div>
+        </div>
+    </dialog>
+</template>
+
+<style scoped>
+
+</style>
