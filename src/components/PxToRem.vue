82c7f139bc7de8a90b1f42cb06eb7e4afdb466f5
Kewish Fagoe <fagoe.kewish@gmail.com>
2025-07-10 01:16:20 -0300

    Refactor: extract reusable Result component

--- src/components/PxToRem.vue
+++ src/components/PxToRem.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue'
 import Toast from './Toast.vue'
 import Card from '../layouts/Card.vue'
+import Result from './Result.vue'
 
 const props = defineProps<{
     rootFontSize: number
@@ -60,23 +61,7 @@ function copyResultToClipboard() {
                 </button>
             </div>
             <Transition name="fade">
-                <div class="mt-4" v-if="showSuccess">
-                    <div class="stat bg-background shadow-sm">
-                        <div class="stat-title">Result</div>
-                        <div class="stat-value text-xl sm:text-3xl">{{ result }} rem</div>
-                        <div class="stat-actions flex justify-end">
-                            <button
-                                class="btn btn-neutral btn-sm"
-                                @click="copyResultToClipboard"
-                            >
-                                <span>Copy</span>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-copy" viewBox="0 0 16 16">
-                                    <path fill-rule="evenodd" d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z"/>
-                                </svg>
-                            </button>
-                        </div>
-                    </div>
-                </div>
+                <Result v-if="showSuccess" :result="result" unit="rem" @copy-result="copyResultToClipboard" />
             </Transition>
         </template>
     </Card>
---
+++ src/components/Result.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+
+const props = defineProps<{
+    result: number
+    unit: string
+}>()
+
+const emit = defineEmits<{
+    copyResult: []
+}>()
+
+function copyResult() {
+    emit("copyResult")
+}
+
+</script>
+
+<template>
+    <div class="mt-4">
+        <div class="stat bg-background shadow-sm">
+            <div class="stat-title">Result</div>
+            <div class="stat-value text-xl sm:text-3xl">{{ props.result }} {{ props.unit }}</div>
+            <div class="stat-actions flex justify-end">
+                <button
+                    class="btn btn-neutral btn-sm"
+                    @click="copyResult"
+                >
+                    <span>Copy</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-copy" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z"/>
+                    </svg>
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
